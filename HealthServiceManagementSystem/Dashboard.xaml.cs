2971d90474baffb948b2d2779f7421bb39659f21
Paul W <L00150252@student.lyit.ie>
2018-11-04 14:48:13 +0000

    Page updated

--- HealthServiceManagementSystem/Dashboard.xaml.cs
+++ HealthServiceManagementSystem/Dashboard.xaml.cs
@@ -26,5 +26,16 @@ namespace HealthServiceManagementSystem
             
 
         }
+
+        private void btnAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            Admin admin = new Admin();
+            adminFrame.Navigate(admin);
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
